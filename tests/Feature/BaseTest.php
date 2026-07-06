a7fe148e3f4e26f268f10f770b2d276005099b37
Baran Arda <baran9arda@gmail.com>
2026-07-06 13:16:52 +0300

    Support Laravel v13

--- tests/Feature/BaseTest.php
+++ tests/Feature/BaseTest.php
@@ -2,6 +2,7 @@
 
 namespace Theposeidonas\LaravelParasutApi\Tests\Feature;
 
+use Illuminate\Support\Facades\Cache;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 abstract class BaseTest extends OrchestraTestCase
@@ -14,13 +15,15 @@ abstract class BaseTest extends OrchestraTestCase
             \Dotenv\Dotenv::createImmutable(__DIR__.'/../../')->load();
         }
 
-        config()->set('parasut.username', env('PARASUT_USERNAME'));
-        config()->set('parasut.password', env('PARASUT_PASSWORD'));
-        config()->set('parasut.company_id', env('PARASUT_COMPANY_ID'));
-        config()->set('parasut.client_id', env('PARASUT_CLIENT_ID'));
-        config()->set('parasut.client_secret', env('PARASUT_CLIENT_SECRET'));
-        config()->set('parasut.redirect_uri', env('PARASUT_REDIRECT_URI'));
+        config()->set('parasut.username', env('PARASUT_USERNAME', 'test_username'));
+        config()->set('parasut.password', env('PARASUT_PASSWORD', 'test_password'));
+        config()->set('parasut.company_id', env('PARASUT_COMPANY_ID', 'test_company'));
+        config()->set('parasut.client_id', env('PARASUT_CLIENT_ID', 'test_client_id'));
+        config()->set('parasut.client_secret', env('PARASUT_CLIENT_SECRET', 'test_secret'));
+        config()->set('parasut.redirect_uri', env('PARASUT_REDIRECT_URI', 'https://localhost'));
         config()->set('parasut.api_url', env('PARASUT_API_URL', 'https://api.parasut.com/v4/'));
+
+        Cache::put('parachute_token', 'test_token', 3600);
     }
 
     protected function getPackageProviders($app): array
